eb23a8d2fc184efc422d7b1d948a0fe85ab89f9c
Noel Lozano <noelmlozano6@gmail.com>
2025-07-17 09:59:04 -0600

    created pantry.html and casted to integer for the sorts

--- app/routes/pantry_display.py
+++ app/routes/pantry_display.py
@@ -1,5 +1,6 @@
 from app.models.pantry import Pantry
 from flask import render_template, Blueprint, session
+from sqlalchemy import cast, Integer
 
 
 pantry_bp = Blueprint("pantry", __name__)
@@ -12,7 +13,7 @@ def show_pantry():
     if not user_id:
         return "Unauthorized", 401
     
-    eco_sort = Pantry.query.filter_by(user_id=user_id).order_by(Pantry.eco_score.desc()).all()
-    grade_sort = Pantry.query.filter_by(user_id=user_id).order_by(Pantry.score.desc()).all()
+    eco_sort = Pantry.query.filter_by(user_id=user_id).order_by(cast(Pantry.eco_score, Integer).desc()).all()
+    grade_sort = Pantry.query.filter_by(user_id=user_id).order_by(cast(Pantry.score, Integer).desc()).all()
 
     return render_template("pantry.html", eco_sort=eco_sort, grade_sort=grade_sort)
