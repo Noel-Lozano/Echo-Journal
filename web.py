112867326c6f54b948f4f35b14cca7bc1936aaea
Nathan <nathayoha@gmail.com>
2025-07-17 00:34:27 -0500

    login and signup

--- web.py
+++ web.py
@@ -6,29 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return '<h1>About Us</h1>'
-
-@app.route('/features')
-def features():
-    return '<h1>Features</h1>'
-
-@app.route('/reviews')
-def reviews():
-    return '<h1>Reviews</h1>'
-
-@app.route('/contact')
-def contact():
-    return '<h1>Contact</h1>'
-
 @app.route('/login')
 def login():
-    return '<h1>Login</h1>'
+    return render_template('login.html')
 
 @app.route('/signup')
 def signup():
-    return '<h1>Sign Up</h1>'
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
